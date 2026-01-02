c14d9bd6409425e3826a3b80f9f6dc4027c69447
littlesam95 <littlesam95@naver.com>
2026-01-02 23:21:21 +0900

    [fix] 매일 10시 00분 30초마다 이벤트 조회하도록 수정

--- src/main/kotlin/com/sixclassguys/maplecalendar/domain/event/scheduler/EventScheduler.kt
+++ src/main/kotlin/com/sixclassguys/maplecalendar/domain/event/scheduler/EventScheduler.kt
@@ -8,14 +8,10 @@ import org.springframework.stereotype.Component
 class EventScheduler(
     private val eventService: EventService
 ) {
+
     // 매일 오전 10시 정각 실행 (초 분 시 일 월 요일)
-    @Scheduled(cron = "5 * * * * *")
+    @Scheduled(cron = "30 0 10 * * *")
     fun scheduleEventUpdate() {
         eventService.refreshAndCheckEvents()
     }
-
-    /* * 테스트를 위해 서버 기동 후 5초 뒤에 바로 한 번 실행해보고 싶다면?
-     * @Scheduled(initialDelay = 5000, fixedDelay = Long.MAX_VALUE)
-     * 를 임시로 붙여두면 바로 확인할 수 있습니다.
-     */
 }
