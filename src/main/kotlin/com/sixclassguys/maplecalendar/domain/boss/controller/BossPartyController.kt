d30cc0b51e55aa7b8c802e4e390cdaf45d3bcc2c
littlesam95 <littlesam95@naver.com>
2026-02-02 02:00:48 +0900

    [fix] 충돌 해결

--- src/main/kotlin/com/sixclassguys/maplecalendar/domain/boss/controller/BossPartyController.kt
+++ src/main/kotlin/com/sixclassguys/maplecalendar/domain/boss/controller/BossPartyController.kt
@@ -1,20 +1,32 @@
 package com.sixclassguys.maplecalendar.domain.boss.controller
 
+import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyAlarmTimeResponse
+import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyChatMessageResponse
 import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyCreateRequest
 import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyCreateResponse
+import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyDetailResponse
+import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyMemberResponse
+import com.sixclassguys.maplecalendar.domain.boss.dto.BossPartyResponse
 import com.sixclassguys.maplecalendar.domain.boss.handler.BossPartyChatWebSocketHandler
 import com.sixclassguys.maplecalendar.domain.boss.service.BossPartyService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springframework.data.domain.Slice
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Boss Party API", description = "보스 파티 관련 정보 조회 API")
 @RestController
 @RequestMapping("/api/boss-parties")
 class BossPartyController(
@@ -23,14 +35,82 @@ class BossPartyController(
 ) {
 
     @PostMapping
-    fun create(@RequestBody @Valid req: BossPartyCreateRequest): ResponseEntity<BossPartyCreateResponse> {
-        val partyId = bossPartyService.createParty(req)
+    fun createBossParty(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @RequestBody @Valid req: BossPartyCreateRequest
+    ): ResponseEntity<BossPartyCreateResponse> {
+        val partyId = bossPartyService.createParty(req, userDetails.username)
         return ResponseEntity.ok(BossPartyCreateResponse(partyId))
     }
 
-    @DeleteMapping("/{messageId}")
+    // 나중에 jwt에 memberId 넣는식으로 고쳐서 쓰는게 편할듯
+    @Operation(summary = "회원이 속한 보스 파티 조회", description = "로그인한 회원의 이메일을 기준으로 가입된 모든 보스 파티 목록을 조회합니다.")
+    @GetMapping
+    fun getBossParties(
+        @AuthenticationPrincipal userDetails: UserDetails
+    ): ResponseEntity<List<BossPartyResponse>> {
+        val bossParties = bossPartyService.getBossParties(userDetails.username)
+
+        return ResponseEntity.ok(bossParties)
+    }
+
+    @GetMapping("/{bossPartyId}")
+    fun getBossPartyDetail(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @PathVariable bossPartyId: Long
+    ): ResponseEntity<BossPartyDetailResponse> {
+        val bossPartyDetail = bossPartyService.getBossPartyDetail(bossPartyId, userDetails.username)
+
+        return ResponseEntity.ok(bossPartyDetail)
+    }
+
+    @Operation(
+        summary = "보스 파티 알람 시간 조회",
+        description = "보스 파티 ID를 기반으로 해당 파티에 설정된 알람 시간 목록을 조회합니다."
+    )
+    @GetMapping("/{bossPartyId}/alarm-times")
+    fun getBossPartyAlarmTimes(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @Parameter(description = "조회할 보스 파티의 ID", required = true)
+        @PathVariable bossPartyId: Long
+    ): List<BossPartyAlarmTimeResponse> {
+        return bossPartyService.getAlarmTimesByBossPartyId(bossPartyId)
+    }
+
+    @Operation(
+        summary = "보스 파티 수락된 멤버 조회",
+        description = "보스 파티 ID를 기반으로 해당 파티에 참여를 수락한 멤버 목록을 조회합니다."
+    )
+    @GetMapping("/{bossPartyId}/members/accepted")
+    fun getAcceptedMembers(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @Parameter(description = "조회할 보스 파티의 ID", required = true)
+        @PathVariable bossPartyId: Long
+    ): List<BossPartyMemberResponse> {
+        return bossPartyService.getAcceptedMembersByBossPartyId(bossPartyId)
+    }
+
+    @Operation(
+        summary = "보스 파티 채팅 메시지 조회",
+        description = "보스 파티 ID를 기반으로 해당 파티에 등록된 채팅 메시지 목록을 조회합니다."
+    )
+    @GetMapping("/{bossPartyId}/chat-messages")
+    fun getChatMessages(
+        @Parameter(description = "조회할 보스 파티의 ID", required = true)
+        @PathVariable bossPartyId: Long,
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "20") size: Int,
+        @AuthenticationPrincipal userDetails: UserDetails
+    ): ResponseEntity<Slice<BossPartyChatMessageResponse>>{
+        val response = bossPartyService.getChatMessages(bossPartyId, userDetails.username, page, size)
+
+        return ResponseEntity.ok(response)
+    }
+
+    @DeleteMapping("/{bossPartyId}/char-messages/{messageId}")
     fun deleteMessage(
         @AuthenticationPrincipal userDetails: UserDetails, // Spring Security 인증 정보
+        @PathVariable bossPartyId: Long,
         @PathVariable messageId: Long
     ): ResponseEntity<Unit> {
         // 1. DB 상태 변경 (isDeleted = true)
