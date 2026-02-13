b1c487c3620cb4771ba843339700e6f6a9d8c046
littlesam95 <littlesam95@naver.com>
2026-02-13 18:34:42 +0900

    [fix] 보스 이름 추가

--- src/main/kotlin/com/sixclassguys/maplecalendar/domain/boss/enums/BossType.kt
+++ src/main/kotlin/com/sixclassguys/maplecalendar/domain/boss/enums/BossType.kt
@@ -1,30 +1,32 @@
 package com.sixclassguys.maplecalendar.domain.boss.enums
 
-enum class BossType {
-    SEREN,              // 선택받은 세렌
-    KALOS,              // 감시자 칼로스
-    THEFIRSTADVERSARY,  // 최초의 대적자
-    KALING,             // 카링
-    RADIANTMALEFIC,     // 찬란한 흉성
-    LIMBO,              // 림보
-    BALDRIX,            // 발드릭스
-    LUCID,              // 루시드
-    WILL,               // 윌
-    DUSK,               // 더스크
-    VERUSHILLA,         // 진 힐라
-    DUNKEL,             // 듄켈
-    BLACKMAGE,          // 검은 마법사
-    ZAKUM,              // 자쿰
-    MAGNUS,             // 매그너스
-    HILLA,              // 힐라
-    PAPULATUS,         // 파풀라투스
-    VONBON,             // 반반
-    PIERRE,             // 피에르
-    BLOODYQUEEN,        // 블러디 퀸
-    VELLUM,             // 벨룸
-    PINKBEAN,           // 핑크빈
-    CYGNUS,             // 시그너스
-    LOTUS,              // 스우
-    DAMIEN,             // 데미안
-    GUARDIANANGELSLIME, // 가디언 엔젤 슬라임
+enum class BossType(
+    val bossName: String
+) {
+    SEREN("선택받은 세렌"),              // 선택받은 세렌
+    KALOS("감시자 칼로스"),              // 감시자 칼로스
+    THEFIRSTADVERSARY("최초의 대적자"),  // 최초의 대적자
+    KALING("카링"),             // 카링
+    RADIANTMALEFIC("찬란한 흉성"),     // 찬란한 흉성
+    LIMBO("림보"),              // 림보
+    BALDRIX("발드릭스"),            // 발드릭스
+    LUCID("루시드"),              // 루시드
+    WILL("윌"),               // 윌
+    DUSK("더스크"),               // 더스크
+    VERUSHILLA("진 힐라"),         // 진 힐라
+    DUNKEL("듄켈"),             // 듄켈
+    BLACKMAGE("검은 마법사"),          // 검은 마법사
+    ZAKUM("자쿰"),              // 자쿰
+    MAGNUS("매그너스"),             // 매그너스
+    HILLA("힐라"),              // 힐라
+    PAPULATUS("파풀라투스"),         // 파풀라투스
+    VONBON("반반"),             // 반반
+    PIERRE("피에르"),             // 피에르
+    BLOODYQUEEN("블러디 퀸"),        // 블러디 퀸
+    VELLUM("벨룸"),             // 벨룸
+    PINKBEAN("핑크빈"),           // 핑크빈
+    CYGNUS("시그너스"),             // 시그너스
+    LOTUS("스우"),              // 스우
+    DAMIEN("데미안"),             // 데미안
+    GUARDIANANGELSLIME("가디언 엔젤 슬라임") // 가디언 엔젤 슬라임
 }
